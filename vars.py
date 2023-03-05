c2836bcae397333d237867112ad8a44aed367bb3
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-03-05 12:54:00 +0530

    Create vars.py

---
+++ vars.py
@@ -0,0 +1,32 @@
+# bot information
+api_id = ''
+api_hash = ''
+bot_token = ''
+
+# bot settings
+cache_time = 300
+pics = ''
+
+# admins, channels & users
+admins = '1584694165' # bot control
+channels = '' # file channel id use @IDFinder_Robot
+
+auth_users = '' # private use (only for auth users)
+auth_group = '' # private use (only for seted chat)
+
+# mongodb information
+database_uri = '' # database url
+database_name = '' # database name
+
+# fsub
+auth_channel = ''
+req_channel = 'request channel id (@IDFinder_Robot)
+join_reqs_db = '' # database url or put this value database_uri
+# join_reqs_db = database_uri
+# join_reqs_db = 'mongodb+srv://jerry:bot@cluster0.qof3hys.mongodb.net/?retryWrites='
+
+# others
+log_channel = '' # log channel id (@IDFinder_Robot)
+support_chat = '' your group link without @ & https://t.me / t.me
+
+
