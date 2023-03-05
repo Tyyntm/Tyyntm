b70eaff311f0be1b968e832c36b68aaf61e113b8
Tyyntm <126965835+Tyyntm@users.noreply.github.com>
2023-03-05 19:58:01 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,32 +1,32 @@
 # bot information
-api_id = ''
-api_hash = ''
-bot_token = ''
+api_id = ''9735241''
+api_hash = ''24abe5c480e78c1ccbe1ece0af06855f''
+bot_token = ''5952559478:AAGRi4zfTHkOtk7JJQ9R6GW2U7uWoXV6ZLo''
 
 # bot settings
 cache_time = 300
-pics = ''
+pics = ''https://telegra.ph/file/313c4cc958238fd0d49a6.jpg''
 
 # admins, channels & users
-admins = '1584694165' # bot control
+admins = '1584694165''5591503031''5688047190'
 channels = '' # file channel id use @IDFinder_Robot
 
 auth_users = '' # private use (only for auth users)
 auth_group = '' # private use (only for seted chat)
 
 # mongodb information
-database_uri = '' # database url
-database_name = '' # database name
+database_uri = ''mongodb+srv://Tyyn:Tyyn@cluster0.gzozj0o.mongodb.net/?retryWrites=true&w=majority''
+database_name = ''Tyysontmefiles''
 
 # fsub
 auth_channel = ''
-req_channel = 'request channel id (@IDFinder_Robot)
+req_channel = '-1001855582592'
 join_reqs_db = '' # database url or put this value database_uri
 # join_reqs_db = database_uri
-# join_reqs_db = 'mongodb+srv://jerry:bot@cluster0.qof3hys.mongodb.net/?retryWrites='
+# join_reqs_db = 'mongodb+srv://Aa:Aa@cluster0.wxgjgbv.mongodb.net/?retryWrites=true&w=majority'
 
 # others
-log_channel = '' # log channel id (@IDFinder_Robot)
+log_channel = ''-1001816438132''
 support_chat = '' your group link without @ & https://t.me / t.me
 
 
