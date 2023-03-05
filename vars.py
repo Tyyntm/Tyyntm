75b998fdb89950174366e8c7ca7aa1ea37388722
Muhammed <74817426+PR0FESS0R-99@users.noreply.github.com>
2023-03-05 20:00:54 +0530

    Update vars.py

--- vars.py
+++ vars.py
@@ -1,32 +1,33 @@
 # bot information
-api_id = ''9735241''
-api_hash = ''24abe5c480e78c1ccbe1ece0af06855f''
-bot_token = ''5952559478:AAGRi4zfTHkOtk7JJQ9R6GW2U7uWoXV6ZLo''
+api_id = '9735241'
+api_hash = '24abe5c480e78c1ccbe1ece0af06855f'
+bot_token = '5952559478:AAGRi4zfTHkOtk7JJQ9R6GW2U7uWoXV6ZLo'
 
 # bot settings
 cache_time = 300
-pics = ''https://telegra.ph/file/313c4cc958238fd0d49a6.jpg''
+pics = 'https://telegra.ph/file/313c4cc958238fd0d49a6.jpg'
 
 # admins, channels & users
-admins = '1584694165''5591503031''5688047190'
+admins = '1584694165 5591503031 5688047190'
 channels = '' # file channel id use @IDFinder_Robot
 
 auth_users = '' # private use (only for auth users)
 auth_group = '' # private use (only for seted chat)
 
 # mongodb information
-database_uri = ''mongodb+srv://Tyyn:Tyyn@cluster0.gzozj0o.mongodb.net/?retryWrites=true&w=majority''
-database_name = ''Tyysontmefiles''
+database_uri = 'mongodb+srv://Tyyn:Tyyn@cluster0.gzozj0o.mongodb.net/?retryWrites=true&w=majority'
+database_name = 'Tyysontmefiles'
 
 # fsub
 auth_channel = ''
 req_channel = '-1001855582592'
 join_reqs_db = '' # database url or put this value database_uri
+# eg
 # join_reqs_db = database_uri
 # join_reqs_db = 'mongodb+srv://Aa:Aa@cluster0.wxgjgbv.mongodb.net/?retryWrites=true&w=majority'
 
 # others
-log_channel = ''-1001816438132''
-support_chat = '' your group link without @ & https://t.me / t.me
+log_channel = '-1001816438132'
+support_chat = 'Im_tyyn' # your group link without @ & https://t.me / t.me
 
 
